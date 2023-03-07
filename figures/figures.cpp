546b864e16ecb42a7ed0096a1949920e194b72ab
MizuruAndFelix <113217149+MizuruAndFelix@users.noreply.github.com>
2023-03-07 18:07:04 +0300

    1

---
+++ figures/figures.cpp
@@ -0,0 +1,56 @@
+﻿#include <fstream>
+#include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+class figure
+{
+protected:
+    string type = "Фигура";
+    int sides = 0;
+public:
+    figure()
+    {
+        sides = 0;
+    }
+    void print_sides()
+    {
+        cout << type << ": " << sides << endl;
+    }
+};
+class triangle: public figure
+{
+public:
+    triangle()
+    {
+        type = "Треугольник";
+        sides = 3;
+    }
+};
+class qangle : public figure
+{
+public:
+    qangle()
+    {
+        type = "Четырехугольник";
+        sides = 4;
+    }
+};
+
+
+int main()
+{
+    setlocale(0, "");
+    SetConsoleCP(1251); //перевод на русский
+    SetConsoleOutputCP(1251);
+    cout << "Количество сторон: \n";
+    figure figure;
+    figure.print_sides();
+    triangle triangle;
+    triangle.print_sides();
+    qangle qangle;
+    qangle.print_sides();
+}
+
